0fe89313587d6d219788778c4d9abf9cca52654d
Jerry Liu <953639086@qq.com>
2022-09-28 18:14:31 +0800

    update unitest output

--- T01/unitest.cpp
+++ T01/unitest.cpp
@@ -7,13 +7,13 @@ int main()
     Triplet a;
     Triplet b;
     Triplet c;
-    InitTriplet(a,1,2,3);
-    InitTriplet(b,1,2,3);
-    InitTriplet(c,1,2,3);
+    InitTriplet(a, 1, 2, 3);
+    InitTriplet(b, 1, 2, 3);
+    InitTriplet(c, 1, 2, 3);
     ElemType i;
     Min(a, i);
-    cout << i<<endl;
+    cout << (i == 1) << endl;
     Add(a, b, c);
-    cout << a[0] << " " << a[1] << " " << a[2] << endl;
+    cout << ((a[0] == 2) && (a[1] == 4) && (a[2] == 8)) << endl;
     return 0;
 }
